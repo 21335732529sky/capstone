638a04dd0766aee881b8d7dd9830cc2f409c355a
Onizuka Lab <onizukalab@OnizukanoMacBook-ea.local>
2018-07-06 12:37:01 +0900

    fix tools for BatchGenerator

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
+
 def read_data(mode, base='USDJPY.csv'):
     filename = os.listdir('Datasets_{}/'.format(mode))
 
@@ -50,24 +51,24 @@ def log_loss(label, logits):
 #----------------------------------------------------------------------
 #                       technical indicator
 #----------------------------------------------------------------------
-def calc_sma(values, window, norm=False):
-    if values.shape[0] < window: return values
-    sma = pd.DataFrame(values).rolling(center=False, window=window).mean()
+def calc_sma(Close, window):
+    if Close.shape[0] < window: return Close
+    sma = pd.DataFrame(Close).rolling(center=False, window=window).mean()
     sma = sma.fillna(method='bfill').values
     return sma.flatten()
 
-def calc_wma(values, window, norm=False):
+def calc_wma(Close, window, norm=False):
     ret = []
-    for i in range(values.shape[0]):
+    for i in range(Close.shape[0]):
         if i < window:
-            ret.append(sum((j+1)*v for j,v in enumerate(values[:i]))/sum(range(1,i+2)))
+            ret.append(sum((j+1)*v for j,v in enumerate(Close[:i])) / sum(range(1, i + 2)))
         else:
-            ret.append(sum((j+1)*v for j,v in enumerate(values[i-window:i]))/sum(range(1,window+1)))
+            ret.append(sum((j+1)*v for j,v in enumerate(Close[i - window:i])) / sum(range(1, window + 1)))
 
-    return np.array(ret) if not norm else np.array(ret) / values
+    return np.array(ret) if not norm else np.array(ret) / Close
 
-def calc_macd(values, span_s, span_l, span_sig):
-    V = pd.DataFrame(values)
+def calc_macd(Close, span_s, span_l, span_sig):
+    V = pd.DataFrame(Close)
     short = pd.DataFrame(pd.ewma(V, span=span_s)).fillna(method='bfill')
     Long = pd.DataFrame(pd.ewma(V, span=span_l)).fillna(method='bfill')
     macd = short-Long
@@ -75,18 +76,18 @@ def calc_macd(values, span_s, span_l, span_sig):
 
     return macd.values.flatten(), macd_sig.values.flatten()
 
-def calc_dmi(values_h, values_l, values_c, window):
+def calc_dmi(High, Low, Close, window):
     ret = []
     pdi = []
     mdi = []
-    for j in range(len(values_h) - window):
-        pDM = sum(values_h[i] - values_h[i-1] if values_h[i] > values_h[i-1]\
+    for j in range(len(High) - window):
+        pDM = sum(High[i] - High[i - 1] if High[i] > High[i - 1]\
                   else 0 for i in range(j+1, window+j+1))
-        mDM = sum(values_l[i-1] - values_l[i] if values_l[i-1] > values_l[i]\
+        mDM = sum(Low[i - 1] - Low[i] if Low[i - 1] > Low[i]\
                   else 0 for i in range(j+1, window+j+1))
-        TR = sum(max(values_h[i] - values_l[i],
-                     values_h[i] - values_c[i-1],
-                     values_c[i-1] - values_l[i])\
+        TR = sum(max(High[i] - Low[i],
+                     High[i] - Close[i - 1],
+                     Close[i - 1] - Low[i])\
                 for i in range(j+1, window+j+1))
         pDI = pDM / TR * 100
         mDI = mDM / TR * 100
@@ -97,18 +98,18 @@ def calc_dmi(values_h, values_l, values_c, window):
            np.array([pdi[0] for _ in range(window)] + pdi),\
            np.array([mdi[0] for _ in range(window)] + mdi)
 
-def calc_roc(values, span):
-    if values.shape[0] <= span:
-        return values
-    ret = [v/u - 1 for v,u in zip(values[span:], values[:-span])]
+def calc_roc(Close, span):
+    if Close.shape[0] <= span:
+        return Close
+    ret = [v / u - 1 for v,u in zip(Close[span:], Close[:-span])]
     return np.array([ret[0] for _ in range(span)] + ret)
 
-def calc_stochastic(values_h, values_l, values_c,
-                   span_x, span_y, span_z):
-    CLn=[values_c[i] - min(values_l[i-span_x:i]) for i in range(span_x, len(values_h))]
+def calc_stochastic(High, Low, Close,
+                    span_x, span_y, span_z):
+    CLn=[Close[i] - min(Low[i - span_x:i]) for i in range(span_x, len(High))]
     CLn = [CLn[0],] * span_x + CLn
-    HnLn=[max(values_h[i-span_x:i]) - min(values_l[i-span_x:i])
-          for i in range(span_x,len(values_h))]
+    HnLn=[max(High[i - span_x:i]) - min(Low[i - span_x:i])
+          for i in range(span_x, len(High))]
     HnLn = [HnLn[0],] * span_x + HnLn
     CLn=pd.DataFrame(CLn).fillna(method='bfill')
     HnLn=pd.DataFrame(HnLn).fillna(method='bfill')
@@ -118,60 +119,60 @@ def calc_stochastic(values_h, values_l, values_c,
 
     return per_k.values.flatten(), per_d.values.flatten(), s_per_d.values.flatten()
 
-def calc_slope(values, span):
+def calc_slope(Close, span):
 
     sumX = sum(range(span))
     sumX2 = sum([x**2 for x in range(span)])
-    sumY = np.array([sum(values[i-span:i]) for i in range(span, values.shape[0]+1)])
-    sumXY = np.array([sum(values[i-span+j]*j for j in range(span))\
-                      for i in range(span, values.shape[0]+1)])
+    sumY = np.array([sum(Close[i - span:i]) for i in range(span, Close.shape[0] + 1)])
+    sumXY = np.array([sum(Close[i - span + j] * j for j in range(span)) \
+                      for i in range(span, Close.shape[0] + 1)])
     ret = [(span*Sxy - sumX*Sy)/(span*sumX2 - sumX**2)\
            for Sy, Sxy in zip(sumY, sumXY)]
 
     return np.array([ret[0],]*(span-1) + ret)
 
-def calc_rsi(values, span):
-    if values.shape[0] < span:
-        return values
-    values = np.array([0] + list(values[1:] - values[:-1]))
-    sumabs = [sum(abs(x) for x in values[i-span:i]) for i in range(span, values.shape[0]+1)]
-    sumup = [sum(x for x in values[i-span:i] if x > 0) for i in range(span, values.shape[0]+1)]
+def calc_rsi(Close, span):
+    if Close.shape[0] < span:
+        return Close
+    Close = np.array([0] + list(Close[1:] - Close[:-1]))
+    sumabs = [sum(abs(x) for x in Close[i - span:i]) for i in range(span, Close.shape[0] + 1)]
+    sumup = [sum(x for x in Close[i - span:i] if x > 0) for i in range(span, Close.shape[0] + 1)]
 
     tmp = [a/b for a,b in zip(sumup, sumabs)]
 
     return np.array([tmp[0],]*(span-1) + tmp)
 
-def calc_bollinger(values, span, y):
+def calc_bollinger(Close, span, y):
     ret = []
-    for i in range(span, values.shape[0]+1):
-        mean = sum(values[i-span:i]) / span
-        std = (sum((v - mean)**2 for v in values[i-span:i]) / span)**0.5
+    for i in range(span, Close.shape[0] + 1):
+        mean = sum(Close[i - span:i]) / span
+        std = (sum((v - mean) ** 2 for v in Close[i - span:i]) / span) ** 0.5
         ret.append(mean + std*y)
     return np.array([ret[0],]*(span-1) + ret)
 
-def calc_cci(values_h, values_l, values_c, span):
+def calc_cci(High, Low, Close, span):
     ret = []
-    pt = [(h + l + c) / 3 for h,l,c in zip(values_h, values_l, values_c)]
-    for i in range(span, values_h.shape[0]+1):
+    pt = [(h + l + c) / 3 for h,l,c in zip(High, Low, Close)]
+    for i in range(span, High.shape[0] + 1):
         psma = sum(pt[i-span:i]) / span
         l1 = sum(abs(p - psma) for p in pt[i-span:i])
         ret.append(1/0.015 * (pt[i-1]-psma)/l1)
 
     return np.array([ret[0],]*(span-1) + ret)
 
-def calc_williams(values, span):
-    tmp =[(values[i-1] - max(values[i-span:i]))/(max(values[i-span:i]) - min(values[i-span:i]))\
-          for i in range(span, values.shape[0]+1)]
+def calc_williams(Close, span):
+    tmp =[(Close[i - 1] - max(Close[i - span:i])) / (max(Close[i - span:i]) - min(Close[i - span:i])) \
+          for i in range(span, Close.shape[0] + 1)]
     return np.array([tmp[0],]*(span-1) + tmp)
 
-def calc_ichimoku(high, low, p_x, p_y, p_z):
-    turning = [(max(high[i-p_x:i]) + min(low[i-p_x:i]))/2 for i in range(p_x, high.shape[0])]
-    base = [(max(high[i-p_y:i]) + min(low[i-p_y:i]))/2 for i in range(p_y, high.shape[0])]
+def calc_ichimoku(High, Low, p_x, p_y, p_z):
+    turning = [(max(High[i - p_x:i]) + min(Low[i - p_x:i])) / 2 for i in range(p_x, High.shape[0])]
+    base = [(max(High[i - p_y:i]) + min(Low[i - p_y:i])) / 2 for i in range(p_y, High.shape[0])]
 
     span1 = [(t + b)/2 for t, b in zip(turning[p_y-p_x:], base)]
     span1 = [0,]*(p_y)*2 + span1
 
-    span2 = [(max(high[i-p_z:i]) + min(low[i-p_z:i]))/2 for i in range(p_z, high.shape[0])]
+    span2 = [(max(High[i - p_z:i]) + min(Low[i - p_z:i])) / 2 for i in range(p_z, High.shape[0])]
 
     span2 = [0,]*(p_y + p_z) + span2
     turning = [0,]*p_x + turning
